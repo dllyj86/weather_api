c4e291ab250261d30bb2909d2d872540d42b29ea
Jimmy Liu <dllyj86@gmail.com>
2020-02-22 14:17:52 +0800

    Try to fix shell issue.

--- scripts/validate.sh
+++ scripts/validate.sh
@@ -1,5 +1,7 @@
-urlstatus=$(curl -s -m 5 -IL http://52.76.30.89:8888/api/weather/citylist|grep 200)
-if [ "$urlstatus" == "HTTP/1.1 200" ];then
+#!/bin/bash
+status=$(curl -s -m 5 -IL http://52.76.30.89:8888/api/weather/citylist|grep 200)
+if [ "$status" == "HTTP/1.1 200" ]
+then
 	exit 0
 else
 	exit 1 
